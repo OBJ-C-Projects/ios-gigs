365febafdbc9879a3bfd1d4f52ba735921875040
Fritz <fritz@icloud.com>
2022-07-29 09:48:03 -0700

    add view controller

---
+++ iOS Gigs/iOS Gigs/View Controllers/GigsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  GigsTableViewController.h
+//  iOS Gigs
+//
+//  Created by FGT MAC on 7/29/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GigsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
