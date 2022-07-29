396dec36789cf0580175eaf0db3192957910e498
Fritz <fritz@icloud.com>
2022-07-29 09:51:59 -0700

    add login VC and scene

---
+++ iOS Gigs/iOS Gigs/View Controllers/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  iOS Gigs
+//
+//  Created by FGT MAC on 7/29/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
