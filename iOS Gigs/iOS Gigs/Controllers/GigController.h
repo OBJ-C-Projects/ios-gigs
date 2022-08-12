127270d5d5bf5eb331705069832a07256aee55d1
Fritz <fritz@icloud.com>
2022-08-12 08:26:19 -0700

    add controller

---
+++ iOS Gigs/iOS Gigs/Controllers/GigController.h
@@ -0,0 +1,16 @@
+//
+//  GigController.h
+//  iOS Gigs
+//
+//  Created by FGT MAC on 8/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GigController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS Gigs/iOS Gigs/Controllers/GigController.m
@@ -0,0 +1,12 @@
+//
+//  GigController.m
+//  iOS Gigs
+//
+//  Created by FGT MAC on 8/12/22.
+//
+
+#import "GigController.h"
+
+@implementation GigController
+
+@end
